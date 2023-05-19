0508b606746775532e0c0dd263bbecb0f63e600f
HBalu1026 <71315012+HBalu1026@users.noreply.github.com>
2023-05-19 11:06:53 +0200

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Pilota.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Pilota
+    {
+        string nev;
+        string datum;
+        string nemzetiseg;
+        int rajtszam;
+
+        public Pilota(string nev, string datum, string nemzetiseg, int rajtszam)
+        {
+            this.nev = nev;
+            this.datum = datum;
+            this.nemzetiseg = nemzetiseg;
+            this.rajtszam = rajtszam;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Datum { get => datum; set => datum = value; }
+        public string Nemzetiseg { get => nemzetiseg; set => nemzetiseg = value; }
+        public int Rajtszam { get => rajtszam; set => rajtszam = value; }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using ConsoleApp7;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("pilotak.csv");
+            List<Pilota> PilotaList = new List<Pilota>();
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string[] sorok = sr.ReadLine().Split(";");
+                if (sorok[3] == "")
+                {
+                    Pilota sor = new Pilota(sorok[0], sorok[1], sorok[2], int.MaxValue);
+                    PilotaList.Add(sor);
+                }
+                else
+                {
+                    Pilota sor = new Pilota(sorok[0], sorok[1], sorok[2], Convert.ToInt32(sorok[3]));
+                    PilotaList.Add(sor);
+                }
+            }
+
+            //3.
+            Console.WriteLine("3 Feladat: " + PilotaList.Count());
+
+            //4.
+            Console.WriteLine("4 Feladat: " + PilotaList[PilotaList.Count() - 1].Nev);
+
+            //5
+            for (int i = 0; i < PilotaList.Count(); i++)
+            {
+                if (Convert.ToInt32(PilotaList[i].Datum.Split(".")[0]) < 1901)
+                {
+                    Console.WriteLine("\t" + PilotaList[i].Nev + "(" + PilotaList[i].Datum + ")");
+                }
+            }
+
+
+
+
+
+
+            sr.Close();
+        }
+    }
+}
